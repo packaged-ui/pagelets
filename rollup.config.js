0d3999265d6a8c554ca0bff4cb3a85f3ea85f986
Tom Kay <oridan82@gmail.com>
2020-05-27 16:47:44 +0100

    bump packages and rebuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import {terser} from "rollup-plugin-terser";
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import {terser} from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 process.chdir(__dirname);
 
@@ -9,7 +9,7 @@ const defaultCfg = {
   output: {
     file: './dist/pagelets.min.js',
     name: 'Pagelets',
-    format: 'iife'
+    format: 'iife',
   },
   plugins: [
     resolve({browser: true, preferBuiltins: false}),
