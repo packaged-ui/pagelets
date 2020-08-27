e8f38c95b520102c9e841876b79f15abf665c7a0
Tom Kay <oridan82@gmail.com>
2020-08-27 10:54:12 +0100

    add helpers to PageletResponse

--- php/src/PageletResponse.php
+++ php/src/PageletResponse.php
@@ -4,6 +4,10 @@ namespace PackagedUI\Pagelets;
 
 use JsonSerializable;
 use PackagedUI\Pagelets\Actions\PageletActionInterface;
+use PackagedUI\Pagelets\Actions\PageletContent;
+use PackagedUI\Pagelets\Actions\PageletLocation;
+use PackagedUI\Pagelets\Actions\PageletRefresh;
+use PackagedUI\Pagelets\Actions\PageletResource;
 
 class PageletResponse implements JsonSerializable
 {
@@ -23,6 +27,42 @@ class PageletResponse implements JsonSerializable
   public function getActions()
   {
     return $this->_data['actions'];
+  }
+
+  public function setContent($content, string $target = '')
+  {
+    $this->addAction(PageletContent::i($content, $target));
+    return $this;
+  }
+
+  public function loadPagelet($target, $url = null)
+  {
+    $this->addAction(PageletRefresh::i($target, $url));
+    return $this;
+  }
+
+  public function windowLocation(string $url, bool $replaceHistory = false, bool $reloadWindow = false)
+  {
+    $this->addAction(PageletLocation::i($url)->replace($replaceHistory)->reload($reloadWindow));
+    return $this;
+  }
+
+  public function redirect($url, bool $replace = false)
+  {
+    $this->windowLocation($url, $replace, true);
+    return $this;
+  }
+
+  public function addJsResource(string $data, bool $isUrl = true)
+  {
+    $this->addAction(PageletResource::js($data)->inline(!$isUrl));
+    return $this;
+  }
+
+  public function addCssResource(string $data, bool $isUrl = true)
+  {
+    $this->addAction(PageletResource::css($data)->inline(!$isUrl));
+    return $this;
   }
 
   public function jsonSerialize()
